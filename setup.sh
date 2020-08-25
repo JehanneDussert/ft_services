c3521743399e00b9a2072d232e885acff05de12b
JehanneDussert <jdussert@student.42.fr>
2020-08-25 16:29:11 +0200

    setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,24 @@
 # Faire condition pour le faire que si minikube est pas allume
 sudo minikube start --driver=none
 sudo chown -R user42 $HOME/.kube $HOME/.minikube
+
+# Ouverture du dashboard web pour gere le cluster
+sudo minikube dashboard & # -> on peut encore se servir du terminal tout en naviguant sur le dashboard
+# minikube addons enable dashboard
+
+# Lancement du loadbalancer
+
+# see what changes would be made, returns nonzero returncode if different
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl diff -f - -n kube-system
+
+# actually apply the changes, returns nonzero returncode on errors only
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
