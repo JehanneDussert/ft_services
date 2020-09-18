6ac4d0f3a6e18012b95f1c87eb41053f2e35d9a5
JehanneDussert <jdussert@student.42.fr>
2020-09-18 12:41:50 +0200

    Need to fix ftps - filezilla

--- srcs/ftps/ftps.sh
+++ srcs/ftps/ftps.sh
@@ -4,10 +4,11 @@ export FTP_USERNAME=42user;
 export FTP_PASSWORD=42pass;
 
 echo -e "$FTP_PASSWORD\n$FTP_PASSWORD" | adduser -h /mnt/ftp $FTP_USERNAME
-
+ssh-keygen -A
 #ftps # lancer le service correctement
 #rc-service ftps start
 #rc-update add vsftpd
 # boucle infinie a refaire
 #tail -f /dev/null
 supervisord
+tail -f /dev/null
