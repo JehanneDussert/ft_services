58e8abe6ef7d777188558b890cf216c867e63518
JehanneDussert <jdussert@student.42.fr>
2020-09-21 15:22:58 +0200

    ftps works

--- srcs/ftps/ftps.sh
+++ srcs/ftps/ftps.sh
@@ -6,11 +6,5 @@ export FTP_PASSWORD=42pass;
 echo -e "$FTP_PASSWORD\n$FTP_PASSWORD" | adduser -h /mnt/ftp $FTP_USERNAME
 mkdir -p /mnt/ftp/doss
 echo "file" > /mnt/ftp/doss/file
-#ssh-keygen -A
-#ftps # lancer le service correctement
-#rc-service ftps start
-#rc-update add vsftpd
-# boucle infinie a refaire
-#tail -f /dev/null
 supervisord
 #tail -f /dev/null
