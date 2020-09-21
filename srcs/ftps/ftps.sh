6d76a8955035350002ffab835028885b17f636f2
JehanneDussert <jdussert@student.42.fr>
2020-09-21 12:18:01 +0200

    Need to fix ftps

--- srcs/ftps/ftps.sh
+++ srcs/ftps/ftps.sh
@@ -4,11 +4,13 @@ export FTP_USERNAME=42user;
 export FTP_PASSWORD=42pass;
 
 echo -e "$FTP_PASSWORD\n$FTP_PASSWORD" | adduser -h /mnt/ftp $FTP_USERNAME
-ssh-keygen -A
+mkdir -p /mnt/ftp/doss
+echo "file" > /mnt/ftp/doss/file
+#ssh-keygen -A
 #ftps # lancer le service correctement
 #rc-service ftps start
 #rc-update add vsftpd
 # boucle infinie a refaire
 #tail -f /dev/null
 supervisord
-tail -f /dev/null
+#tail -f /dev/null
