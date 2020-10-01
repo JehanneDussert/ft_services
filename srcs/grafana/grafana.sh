1aff28c060207093a04bf6cbc8f90b4f15c12c7c
JehanneDussert <jdussert@student.42.fr>
2020-10-01 16:52:02 +0200

    Connexion to grafana works - Dashboards missing

--- srcs/grafana/grafana.sh
+++ srcs/grafana/grafana.sh
@@ -2,6 +2,5 @@
 
 sed s/__GRAF_USER__/$GRAF_USER/g /usr/share/grafana/conf/custom.ini -i
 sed s/__GRAF_PASS__/$GRAF_PASS/g /usr/share/grafana/conf/custom.ini -i
-#grafana-server
-#tail -f /dev/null
+
 supervisord
