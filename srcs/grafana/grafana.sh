e9fe3c53a20c597634a029f1bcb10dfaaad9e1ee
JehanneDussert <jdussert@student.42.fr>
2020-10-01 12:32:52 +0200

    Need to make some changes - influxdb.conf

--- srcs/grafana/grafana.sh
+++ srcs/grafana/grafana.sh
@@ -2,5 +2,6 @@
 
 sed s/__GRAF_USER__/$GRAF_USER/g /usr/share/grafana/conf/custom.ini -i
 sed s/__GRAF_PASS__/$GRAF_PASS/g /usr/share/grafana/conf/custom.ini -i
-
+#grafana-server
+#tail -f /dev/null
 supervisord
--- srcs/influxdb/influxdb.sh
+++ srcs/influxdb/influxdb.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
-supervisord
+influxd
 tail -f /dev/null
+#supervisord
