2e004f7c9fd344b56a3b39f8f857f34a62bc7ba6
JehanneDussert <jdussert@student.42.fr>
2021-02-02 17:00:54 +0100

    Phar package add

--- srcs/wordpress/wordpress.sh
+++ srcs/wordpress/wordpress.sh
@@ -5,11 +5,11 @@ sed s/__DB_USER__/$DB_USER/g /var/www/wordpress/wp-config.php -i
 sed s/__DB_PASSWORD__/$DB_PASSWORD/g /var/www/wordpress/wp-config.php -i
 sed s/__DB_HOST__/$DB_HOST/g /var/www/wordpress/wp-config.php -i
 
-wp core download --path=/var/www/wordpress
+wp core download --path=/var/www/wordpress/
 
 wp core install \
     --path=/var/www/wordpress \
-    --url=192.168.99.103:5050 \
+    --url=172.17.123.1:5050 \
     --title=Title \
     --admin_user=$DB_USER \
     --admin_password=$DB_PASSWORD \
