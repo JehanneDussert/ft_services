10dcdb7cad3960cd20a5cdd1c07c580ac4b2b978
JehanneDussert <jdussert@student.42.fr>
2021-02-16 15:13:03 +0100

    ftps works

--- srcs/ftps/ftps.sh
+++ srcs/ftps/ftps.sh
@@ -5,6 +5,5 @@ export FTP_PASSWORD=42pass;
 
 echo -e "$FTP_PASSWORD\n$FTP_PASSWORD" | adduser -h /mnt/ftp $FTP_USERNAME
 mkdir -p /mnt/ftp/doss
-echo "file" > /mnt/ftp/doss/file
+echo "success" > /mnt/ftp/doss/file
 supervisord
-#tail -f /dev/null
