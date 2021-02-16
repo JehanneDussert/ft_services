187f71e9a76e7fe1baa6e865c8ebc07e9f4e4193
JehanneDussert <jdussert@student.42.fr>
2021-02-16 16:34:58 +0100

    Try to fix wp appearance

--- srcs/wordpress/wordpress.sh
+++ srcs/wordpress/wordpress.sh
@@ -8,17 +8,4 @@ sed s/__DB_HOST__/$DB_HOST/g /var/www/wordpress/wp-config.php -i
 rm -rf /var/www/html/wordpress
 mv /var/www/wordpress/ /var/www/html
 
-# wp core download --path=/var/www/html/wordpress/
-
-# wp core install \
-#      --path=/var/www/html/wordpress \
-#      --url=localhost:5050 \
-#      --title=Title \
-#      --admin_user=$DB_USER \
-#      --admin_password=$DB_PASSWORD \
-#      --admin_email="admin@admin.com"
-
-# wp user create user1 test1@mail.com --role=contributor --path=/var/www/html/wordpress --user_pass=user1
-# wp user create user2 test2@mail.com --role=editor --path=/var/www/html/wordpress  --user_pass=user2
-
 supervisord
